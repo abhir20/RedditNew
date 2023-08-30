a3089617ad9fc4fd02528064372e56e6b470f44b
Abhinaya Rangarajan <rabhinaya0@gmail.com>
2023-08-30 01:48:22 -0400

    Added test project

--- ServiceLayer/Implementation/RedditClient.cs
+++ ServiceLayer/Implementation/RedditClient.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Reddit.Controllers;
+using Reddit.Controllers.EventArgs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,9 @@ using System.Threading.Tasks;
 
 namespace ServiceLayer.Implementation
 {
-    interface RedditClient
+    public class RedditClient
     {
+
+
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
