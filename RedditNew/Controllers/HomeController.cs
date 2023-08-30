fcd6d01f94b9895c0d7d098ca76dc20b6d0d0f0b
Abhinaya Rangarajan <rabhinaya0@gmail.com>
2023-08-29 22:36:56 -0400

    Added DAL project

--- RedditNew/Controllers/HomeController.cs
+++ RedditNew/Controllers/HomeController.cs
@@ -33,5 +33,6 @@ namespace RedditNew.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
--- RedditNew/Controllers/PostsController.cs
+++ RedditNew/Controllers/PostsController.cs
@@ -15,19 +15,25 @@ namespace RedditNew.Controllers
     [ApiController]
     public class PostsController : ControllerBase
     {
+        private readonly RedditClient reddit = null;
+        PostsController(RedditClient _reddit)
+        {
+            reddit = new RedditClient(appId: appId, refreshToken: refreshToken, accessToken: accessToken);
+        }
 
         static string appId = "77Vhm2YGgk703wHWBmX9ig";
         static string refreshToken = "760730588485-UfbKjs0RFOSqsyz-_rEG3iHHtgBROg";
         static string accessToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjkzNDI2MTE3Ljk5NzUwNSwiaWF0IjoxNjkzMzM5NzE3Ljk5NzUwNSwianRpIjoiQmFkbDNiZDF4RUlQdkRpcHVZOGNoZUtzeWJvdS1BIiwiY2lkIjoiNzdWaG0yWUdnazcwM3dIV0JtWDlpZyIsImxpZCI6InQyXzlwaDNjbXJwIiwiYWlkIjoidDJfOXBoM2NtcnAiLCJsY2EiOjE2MTAwMzUzMTMwMDAsInNjcCI6ImVKeUtWdEpTaWdVRUFBRF9fd056QVNjIiwiZmxvIjo5fQ.kkMKzKh7BtOc9rnjWPn-kurRvU_fgD2U4KBVCePxuiX8YgTzYpBOEUOWp2dNsMLSPxzfK6z5ZOmPsr92Z4fVweDzvUPtG9jN-YvUkVy8EUm-CPGo6SzLBjzGpGOM69SI_yxlwrA1vWAluyv9bOaxALJTiO5AAtyC3mlhBdW9V-AYEr_cnCyivN9rYP4ilfNDBYKRuKN31BSLkg3uDfEcFGQ-df4djhbUyvEJtYyM2h2_geqJB9FBzzsZE7ETglsN8GNSohcRXjBJkULqnKbk92HQwoBmVxPjQHJzf-smwWK7JuKgZZAKExmtMFz3r2nCac33QRmzBuyZMzS8W2TZ-Q";
 
         // Initialize the API library instance.  
-        RedditClient reddit = new RedditClient(appId: appId, refreshToken: refreshToken, accessToken: accessToken);
+       // RedditClient reddit = new RedditClient(appId: appId, refreshToken: refreshToken, accessToken: accessToken);
 
-        [HttpGet]
-        public IActionResult Get()
+        [HttpGet("name")]
+        public async Task<IActionResult> Get(string name)
         {
             try
             {
+                Subreddit subName = reddit.Subreddit(name);
                 Subreddit funny = reddit.Subreddit("funny");
 
                 // Before monitoring, let's grab the posts once so we have a point of comparison when identifying new posts that come in. 
